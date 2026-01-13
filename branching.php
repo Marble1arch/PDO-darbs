06b7fccbdf48aedb48a3f5dfe5f44a70b0704944
ipb24.r.terauds <ipb24.r.terauds@vtdt.edu.lv>
2026-01-13 11:28:46 +0200

    filler

---
+++ branching.php
@@ -0,0 +1,5 @@
+<?php 
+require_once "functions.php";
+//$x = "Kaķēni";
+
+dd(isset($x));
---
+++ views/index.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="lv">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Emuārs</title>
+</head>
+<body>
+</ul>
+    <h1>Emuārs</h1>
+    <form>
+        <input name='search_query' />
+        <button>Meklēt</button>
+    </form>
+    <?php if(count($posts)==0) { ?>
+        <p>❌ Nav atrasts neviens ieraksts. 😭 Lūdzu, pamēģini citu vārdu vai frāzi 🐣</p>
+    <?php } else {?>
+    <ul>
+       <?php foreach($posts as $post) { ?>
+            <li><?= $post["content"]?></li>
+       <?php } ?>
+    </ul>
+        <?php } ?>
+</body>
+</html>
