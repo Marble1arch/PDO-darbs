d39645ae3f96e12f3a03e4ca6ec929259e79d76f
ipb24.r.terauds <ipb24.r.terauds@vtdt.edu.lv>
2026-02-02 15:19:51 +0200

    fill

--- controllers/posts/create.php
+++ controllers/posts/create.php
@@ -1,14 +1,19 @@
 <?php
+require "Validator.php";
 $pageTitle = "Emuārs - Izveidot";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $sql = "INSERT INTO posts (content)
+    $errors = [];
+    if(!Validator::string($_POST["content"],max: 50)){
+        $errors["content"] = "Saturam jābūt ievadītam, bet ne garākam par 50 rakstzīmēm";
+    }
+    if (empty($errors)){
+        $sql = "INSERT INTO posts (content)
         VALUES(:name)";
-    $params = [
-        "name" => $_POST["content"],
-    ];
+    $params = ["name" => $_POST["content"],];
     $db->query($sql, $params);
     header("Location: /");
     exit();
-    $errors = [];
+        header("Location: /");
+    }
 }
 require "views/posts/create.view.php";
--- views/posts/create.view.php
+++ views/posts/create.view.php
@@ -2,7 +2,14 @@
 <?php require "./views/components/navbar.php";?>
     <h1>Izveidot bloga ierakstu</h1>
     <form method="POST">
-    <label>Bloga raksts<input name="content"/></label><br/>
+
+    <label>Bloga raksts<input name="content" value ='<?= $_POST['content'] ?? "" ?>'/></label><br/>
+
+    <?php if(isset($errors["content"])){?>
+     <p><?=$errors["content"]?></p>
+    <?php } ?></br>
+
     <button>Izveidot</button>
     </form>
+
 <?php require "./views/components/footer.php";?>
